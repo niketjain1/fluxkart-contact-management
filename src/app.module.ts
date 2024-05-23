2acdbdaefc7e7a0d696594d9f18209b4f6dbeff5
niketjain1 <niketj2000@gmail.com>
2024-05-23 23:34:17 +0530

    👌 IMPROVE: update app module file to the required configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.enitity.js')],
+        synchronize: false
+      })
+    }),
+    ContactsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
